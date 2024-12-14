ee0074f92b7aa7bcfe6e6ce6af6a628083f42496
ifrair <Palex2003.02@mail.ru>
2024-12-14 15:27:51 +0300

    fix all style

--- calendar_util.py
+++ calendar_util.py
@@ -1,11 +1,12 @@
 import logging
 import os
-import google.auth
+from google.auth.transport.requests import Request as GoogleRequest
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from datetime import datetime
 from util import push_to_json, take_from_json
+
 
 # gets date of last interaction with recount function
 def get_last_date_time():
@@ -20,6 +21,7 @@ def get_last_date_time():
         date_of_last_recount["second"]).isoformat() + 'Z'
     return date
 
+
 # gets date from datetime.now()
 def get_now_date():
     date_now = datetime.now()
@@ -31,6 +33,7 @@ def get_now_date():
         date_now.minute,
         date_now.second).isoformat() + 'Z'
     return date
+
 
 # updates last interaction date with recount
 def last_date_update(date_now):
@@ -44,21 +47,22 @@ def last_date_update(date_now):
     date_of_last_recount["second"] = date_now.second
     push_to_json(config_json["last_time"], date_of_last_recount)
 
-#gets services of google calendar
+
+# gets services of Google calendar
 def get_service():
-    SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+    url = ['https://www.googleapis.com/auth/calendar.readonly']
 
     config_json = take_from_json("config.json")
     creds = None
     if os.path.exists(config_json["google_token"]):
         creds = Credentials.from_authorized_user_file(
-            config_json["google_token"], SCOPES)
+            config_json["google_token"], url)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(google.auth.transport.requests.Request())
+            creds.refresh(GoogleRequest())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                config_json["credentials"], SCOPES)
+                config_json["credentials"], url)
             creds = flow.run_local_server(
                 port=0, access_type='offline', prompt='consent')
         creds_to_json = creds.to_json()
@@ -66,42 +70,46 @@ def get_service():
     service = build('calendar', 'v3', credentials=creds)
     return service
 
+
 # processes the event and return errors (if they exist)
 def processing_event(event):
-    errors = ''
     config_json = take_from_json("config.json")
-    babki_json = take_from_json(config_json["money_count"])
+    money_json = take_from_json(config_json["money_count"])
     event_start = event['start'].get('dateTime', event['start'].get('date'))
     summary = event.get('summary', 'Нет названия')
     description = event.get('description', 'Нет описания')
     if summary == "Нет названия":
         return ''
-    else:
-        summary_list = list(summary.split())
-        if ("урок" in summary_list) or ("Урок" in summary_list):
-            if not description.isnumeric():
-                logging.error(
-                    f'НЕПРАВИЛЬНЫЙ ФОРМАТ ОПИСАНИЯ!\n {summary};\n Дата занятия: {event_start};\n Описание - {description}')
-                errors += f'НЕПРАВИЛЬНЫЙ ФОРМАТ ОПИСАНИЯ!\n {summary};\n Дата занятия: {event_start};\n Описание - {description}\n\n'
-                return errors
-            if summary_list[0] not in babki_json:
-                babki_json[summary_list[0]] = -int(description)
+
+    error_text = f"""НЕПРАВИЛЬНЫЙ ФОРМАТ ОПИСАНИЯ!
+      {summary};
+      Дата занятия: {event_start};
+      Описание - {description}"""
+    summary_list = list(summary.split())
+    description_list = list(description.split())
+
+    if ("урок" in summary_list) or ("Урок" in summary_list):
+        money_count = description_list[0]
+        name = summary_list[0]
+        if not money_count.isnumeric():
+            logging.error(error_text)
+            return error_text
+        money_count = int(money_count)
+        if name not in money_json:
+            money_json[name] = 0
+        money_json[name] -= money_count
+    elif ("Группа" in summary_list) or ("группа" in summary_list):
+        for i in range(0, len(description_list) - 1, 2):
+            if not description_list[i + 1].isnumeric():
+                logging.error(error_text)
+                return error_text
+            if description_list[i] not in money_json:
+                money_json[description_list[i]] = -int(description_list[i + 1])
             else:
-                babki_json[summary_list[0]] -= int(description)
-        elif ("Группа" in summary_list) or ("группа" in summary_list):
-            description_list = list(description.split())
-            for i in range(0, len(description_list) - 1, 2):
-                if not description_list[i + 1].isnumeric():
-                    logging.error(
-                        f'НЕПРАВИЛЬНЫЙ ФОРМАТ ОПИСАНИЯ!\n {summary};\n {i};\n Дата занятия: {event_start};\n Описание - {description}\n\n')
-                    errors += f'НЕПРАВИЛЬНЫЙ ФОРМАТ ОПИСАНИЯ!\n {summary};\n {i};\n Дата занятия: {event_start};\n Описание - {description}\n\n'
-                    return errors
-                if description_list[i] not in babki_json:
-                    babki_json[description_list[i]] = -int(description_list[i + 1])
-                else:
-                    babki_json[description_list[i]] -= int(description_list[i + 1])
-    push_to_json(config_json["money_count"], babki_json)
-    return errors
+                money_json[description_list[i]] -= int(description_list[i + 1])
+    push_to_json(config_json["money_count"], money_json)
+    return ''
+
 
 def recount():
     config_json = take_from_json("config.json")
